ebc53149814b94eca8a06a54de4425c4923fdd1d
Gabriel Vasconcelos <92489005+gabrielsvasconcelos@users.noreply.github.com>
2023-03-19 22:02:35 -0300

    feat: início header
    header adicionado no layout main. Links para diferentes páginas foram adicionados.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,9 +13,28 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <!-- js e css -->
         <script src="/js/script.js"></script>
-        <style src="style.css"></style>
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <nav class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/imagens/logo.png" alt="Vasconcelos Eventos">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                </nav>
+            </nav>
+        </header>
         @yield('conteudo')
     </body>
     <footer>
