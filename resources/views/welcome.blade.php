0a3f2491c6343c4c52634d07e3c61b920d6c8964
Gabriel Vasconcelos <92489005+gabrielsvasconcelos@users.noreply.github.com>
2023-03-21 21:53:07 -0300

    feat: criação e estilização da página home
    Campo de busca de evento, cards listando eventos e rodapé foram criados
    
    Todos esses componentes foram estilizados utilizando bootstrap e css puro.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,11 +19,35 @@
             <p>{{$item}}</p>
             
         @endforeach
---}}
+
     <h1>Página Inicial</h1>
     <h2>Lista de eventos</h2>
     @foreach($eventos as $evento)
         <p>Nome: {{$evento->titulo}} - Descrição: {{$evento->descricao}}</p>
-    @endforeach
 
+    @endforeach
+--}}
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar eventos">
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p class="subtitle">Veja os eventos nos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach($eventos as $evento)
+            <div class="card col-md-3">
+                <img src="imagens/festa.jpg" alt="{{$evento->titulo}}">
+                <div class="card-body">
+                    <p class="card-body">01/01/2023</p>
+                    <h5 class="card-title">{{$evento->titulo}}</h5>
+                    <p class="card-participants">X participantes</p>
+                    <a href="#" class="btn btn-primary">Saiba Mais</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
